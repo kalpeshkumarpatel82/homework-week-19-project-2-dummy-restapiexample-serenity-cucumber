a141119437c83183e48ffbf2f1d1d1c8be66f235
kalpeshkumarpatel82 <kalpeshkumar.patel@gmail.com>
2022-06-19 00:39:42 +0200

    homework-week-19-project-2

---
+++ src/test/java/com/restapiexample/dummy/cucumber/TestRunner.java
@@ -0,0 +1,15 @@
+package com.restapiexample.dummy.cucumber;
+/* 
+ Created by Kalpesh Patel
+ */
+
+import com.restapiexample.dummy.testbase.TestBase;
+import cucumber.api.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(features = "src/test/java/resources/feature",
+        tags = "@crud")
+public class TestRunner extends TestBase {
+}
